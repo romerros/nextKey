82cef06da5c1ce51b67734e4883df4286f37634a
romerros <r.avetikyan@gmail.com>
2016-01-17 16:12:59 +0400

    17,01,15

--- js/function.js
+++ js/function.js
@@ -1,19 +1,19 @@
-function getNextLockNum(doors) {
-    for (var i = 0; i < doors.length; i++) {
-          if (i == 0 && doors[i] == 0 && doors[i+1] == 0) {
+function getNextLockerNumber(lockers) {
+    for (var i = 0; i < lockers.length; i++) {
+          if (i == 0 && lockers[i] == 0 && lockers[i+1] == 0) {
               return i;
           }
-          if (doors[i] == 0 && doors[i-1] == 0 && doors[i+1] == 0) {
+          if (lockers[i] == 0 && lockers[i-1] == 0 && lockers[i+1] == 0) {
               return i;
           }
-          if (i == doors.length -1 && doors[i] == 0 && doors[i-1] == 0) {
+          if (i == lockers.length -1 && lockers[i] == 0 && lockers[i-1] == 0) {
               return i;
           }
     }
-        for (i = 0; i < doors.length; i++) {
-              if (doors[i] == 0) {
-                return i;
-              }
-        }
+    for (i = 0; i <= lockers.length; i++) {
+          if (lockers[i] == 0) {
+              return i;
+          }
+    }
     return -1;
 }
---
+++ js/index.js
@@ -0,0 +1,47 @@
+$(document).on('ready', function() {
+    
+    
+    
+    $('button.nextLocker').on('click', function() {
+        var $lockers = $('div.locker');
+        var massivForFunction = [];
+        var $index;
+        for (var i = 0; i < $lockers.length; i++) {
+            massivForFunction[i] = $lockers.eq(i).data("lockerstatus");
+        }
+          
+            $index = getNextLockerNumber(massivForFunction);
+            selectLocker($lockers.eq($index));
+        
+        if ($index == -1) {
+            alert('No free lockers found');
+            $('button.nextLocker').attr('disabled', "disabled").addClass("button--disabled");
+        };
+    });
+    
+    $("div.locker").on('click', function() {
+         deSelectLocker($(this));
+        $('button.nextLocker').removeAttr("disabled").removeClass("button--disabled");
+    });
+    //Adding new locker by button (+)
+    $('button.addLocker').on('click', function() {
+        var newDiv = document.createElement('div');
+        newDiv.className = 'locker';
+        var $summeryLockersNow = $('div.locker');
+        for (j = 0; j < $summeryLockersNow.length; j++) {
+            var k = j + 2;
+            newDiv.innerHTML = k;
+        }
+        document.body.appendChild(newDiv);
+    })
+});
+
+function selectLocker($lockers) {
+    $lockers.addClass('locker--locked');
+    $lockers.data('lockerstatus', 1);
+};
+
+function deSelectLocker($lockers) {
+    $lockers.removeClass('locker--locked');
+    $lockers.data('lockerstatus', 0);   
+};
